88c01b4144c173129f3f67bf3fbc51500d761ced
user <user@rcfproj.aws.selectica.net>
2015-06-18 22:10:05 +0000

    Base Thu Jun 18 22:10:02 UTC 2015

---
+++ src/main/java/com/selectica/Base/definitions/CPROCBO/conditions/ConditionCustomerUseEsignature.java
@@ -0,0 +1,13 @@
+package com.selectica.Base.definitions.CPROCBO.conditions;
+
+import com.selectica.rcfscripts.AbstractBOReadScript;
+
+/**null*/
+public class ConditionCustomerUseEsignature extends AbstractBOReadScript<Boolean> {
+
+    @Override
+    public Boolean process() {
+        //TODO generated;
+        return null;
+    }
+}
